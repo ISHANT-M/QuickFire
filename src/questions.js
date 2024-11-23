1f68e59bf7cde47dcd892b88701bb988a70f8383
Ishant Mehndiratta <MEHNDIRATTA.ISHANT2006@GMAIL.COM>
2024-11-24 00:51:24 +0530

    Create questions.js

---
+++ src/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        "question": "Who is the General Secretary of MLSC?",
+        "options": ["Tanmay Bhaiya", "Agrim Bhaiya", "Prisha Didi", "Vansh Bhaiya"],
+        "answer": 0
+    },
+    {
+        "question": "Whose idea was it to name the media department 'MMS'?",
+        "options": ["Bill Gates himself", "Vanika Didi", "Navdeep Bhaiya", "Preksha Didi"],
+        "answer": 2
+    },
+    {
+        "question": "What does the word 'Supercalifragilisticexpialidocious' mean?",
+        "options": ["Very bad", "Exceptionally good", "Extremely hot", "We just made this up"],
+        "answer": 1
+    },
+    {
+        "question": "Who is known as the 'Father of the Nation'?",
+        "options": ["Narendra Modi", "Godse", "Jawaharlal Nehru", "M. K. Gandhi"],
+        "answer": 3
+    },
+    {
+        "question": "Who is the current President of India?",
+        "options": ["Narendra Modi", "Donald Trump", "Droupadi Murmu", "Salman Khan"],
+        "answer": 2
+    },
+    {
+        "question": "What is the name of the cartoon, very popular among Gen Z, that has a camera as the main antagonist?",
+        "options": ["Rizzeler Camera", "Skibidi Toilet", "Attack on Titan", "Shin-Chan"],
+        "answer": 1
+    },
+    {
+        "question": "What is the primary function of a servo motor in robotics projects?",
+        "options": ["To measure temperature", "To provide precise angular movement", "To generate ultrasonic signals", "To detect obstacles"],
+        "answer": 1
+    },
+    {
+        "question": "In *Interstellar*, what is the name of the robot that accompanies Cooper and his crew on their mission?",
+        "options": ["HAL", "TARS", "WALL-E", "EVA"],
+        "answer": 1
+    },
+    {
+        "question": "What is the symbolic meaning of the green light in *The Great Gatsby*?",
+        "options": ["Hope and dreams", "Wealth and status", "Love and betrayal", "Freedom and power"],
+        "answer": 0
+    },
+    {
+        "question": "In *Dead Poets Society*, what phrase does Mr. Keating use to inspire his students to seize the day?",
+        "options": ["Carpe diem", "Veni, vidi, vici", "Memento mori", "Ad astra"],
+        "answer": 0
+    }
+];
